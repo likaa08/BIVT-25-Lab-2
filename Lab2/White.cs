a58228aa48e504465e71bc171a7e9ab91e0f94b4
likaa08 <14angel_v14@mail.ru>
2025-10-05 10:32:28 +0300

    Update White.cs

--- Lab2/White.cs
+++ Lab2/White.cs
@@ -1,4 +1,6 @@
-﻿namespace Lab2
+using static System.Net.WebRequestMethods;
+
+namespace Lab2
 {
     public class White
     {
@@ -8,7 +10,7 @@
             int answer = 0;
 
             // code here
-
+            answer = (((2 + (3 * n - 1)) * n) / 2);
             // end
 
             return answer;
@@ -18,7 +20,10 @@
             double answer = 0;
 
             // code here
-
+            for(int i = 1; i <= n; i++)
+            {
+                answer += 1.0 / i;
+            }
             // end
 
             return answer;
@@ -28,17 +33,29 @@
             long answer = 0;
 
             // code here
+            if (n == 0)
+            {
+                answer = 1;
+            }
+            else
+            {
+                answer = n * Task3(n - 1);
+            }
 
-            // end
+                // end
 
-            return answer;
+                return answer;
         }
         public long Task4(int a, int b)
         {
             long answer = 0;
 
             // code here
-
+            answer = 1;
+            for(int i = 0; i < b; i++)
+            {
+                answer *= a;
+            }
             // end
 
             return answer;
@@ -48,7 +65,14 @@
             int answer = 0;
 
             // code here
-
+            int p = 1;
+            int a = 1;
+            while (p <= L)
+            {
+                p = p * a;
+                a = a + 3;
+            }
+            answer = a - 3;
             // end
 
             return answer;
@@ -59,7 +83,14 @@
             const double R = 6371.0; // радиус Земли, км
 
             // code here
-
+            double eps = 1e-4;
+            double term = 1;
+            double x2 = x * x;
+            while (term >= eps)
+            {
+                answer += term;
+                term *= x2;
+            }
             // end
 
             return answer;
@@ -70,7 +101,14 @@
             int answer = 0;
 
             // code here
+            int sum = 0;
+            while (sum < n)
+            {
+                answer++;
+                sum += answer;
+            }
 
+           
             // end
 
             return answer;
@@ -78,9 +116,10 @@
         public int Task8(double L, double v)
         {
             int answer = 0;
-
+            const double R = 6371.0;
             // code here
-
+            double h = Math.Sqrt(R * R + L * L) - R;
+            answer = (int)(Math.Ceiling(h / v));
             // end
 
             return answer;
